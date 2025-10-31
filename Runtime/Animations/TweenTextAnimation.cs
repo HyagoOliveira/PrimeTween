aac727c685df65b78985ba0307981c85b04a39cb
Hyago Oliveira <hyagogow@gmail.com>
2025-10-31 12:44:38 -0300

    Using TEXT_MESH_PRO_INSTALLED

--- Runtime/Animations/TweenTextAnimation.cs
+++ Runtime/Animations/TweenTextAnimation.cs
@@ -9,17 +9,21 @@ namespace PrimeTween
     [DisallowMultipleComponent]
     public sealed class TweenTextAnimation : AbstractTweenAnimation<int>
     {
+#if TEXT_MESH_PRO_INSTALLED
         [Space]
         [SerializeField] private TMP_Text target;
+#endif
 
         private void Reset() => target = GetComponentInChildren<TMP_Text>();
 
         public override void PlayAnimation()
         {
+#if TEXT_MESH_PRO_INSTALLED
             settings.endValue = System.Math.Max(settings.endValue, target.text.Length);
 
             target.ForceMeshUpdate();
             Tween.TextMaxVisibleCharacters(target, settings);
+#endif
         }
     }
 }
