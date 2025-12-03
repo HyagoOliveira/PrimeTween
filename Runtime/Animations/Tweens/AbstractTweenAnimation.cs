501af8a8e646c99aee1386a5b12ec996f54b7dcf
Hyago Oliveira <hyagogow@gmail.com>
2025-12-03 19:06:16 -0300

    Make tween animation able to loop

--- Runtime/Animations/Tweens/AbstractTweenAnimation.cs
+++ Runtime/Animations/Tweens/AbstractTweenAnimation.cs
@@ -8,15 +8,24 @@ namespace PrimeTween
     /// <typeparam name="T"></typeparam>
     public abstract class AbstractTweenAnimation<T> : AbstractAnimation where T : struct
     {
-        [Tooltip("The Tween animation properties.")]
+        [Tooltip("The Tween animation properties."), ContextMenuItem("Create Loop", nameof(SetLoop))]
         public TweenSettings<T> settings;
 
         protected Tween tweenAnimation = default;
 
         private void OnEnable() => Play();
+        private void OnDisable() => Stop();
 
         public override void Play() => tweenAnimation = GetTweenAnimation();
         public override void Stop() => tweenAnimation.Stop();
+
+        public void SetLoop(CycleMode loop = CycleMode.Yoyo)
+        {
+            settings.settings.cycles = -1;
+            settings.settings.cycleMode = loop;
+
+            if (Application.isPlaying) tweenAnimation.SetRemainingCycles(-1);
+        }
 
         protected abstract Tween GetTweenAnimation();
     }
