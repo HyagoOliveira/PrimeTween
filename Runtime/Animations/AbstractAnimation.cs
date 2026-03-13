7666f9435b666783a9c91f19654d40a276989013
Hyago Oliveira <hyagogow@gmail.com>
2026-03-13 11:18:51 -0300

    Add animation identifier

--- Runtime/Animations/AbstractAnimation.cs
+++ Runtime/Animations/AbstractAnimation.cs
@@ -4,6 +4,8 @@ namespace PrimeTween
 {
     public abstract class AbstractAnimation : MonoBehaviour
     {
+        public string identifier = string.Empty;
+
         public abstract Awaitable PlayAsync();
         public abstract void Play();
         public abstract void Stop();
